bce37adad69fb8a69e9bbe765ba684085e57337c
AVT30 <123390487+AVT30@users.noreply.github.com>
2023-02-23 10:20:54 +0100

    .

--- resources/views/layout/page.blade.php
+++ resources/views/layout/page.blade.php
@@ -7,6 +7,8 @@
     <title>Document</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+    <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
 </head>
 <body>
     {{-- Ajout d'un include ici pour le header et le footer pour eviter la repetition de code  --}}

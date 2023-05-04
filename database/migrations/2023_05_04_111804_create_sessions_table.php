c6c7480f473db688189bcc07295657271df8bec3
AVT30 <123390487+AVT30@users.noreply.github.com>
2023-05-04 13:27:57 +0200

    s

---
+++ database/migrations/2023_05_04_111804_create_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index();
+            $table->string('ip_address');
+            $table->string('user_agent');
+            $table->text('payload');
+            $table->bigInteger('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
--- resources/views/chaussures/chaussure/show.blade.php
+++ resources/views/chaussures/chaussure/show.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.page')
+<script>
+    function highlightStar(starNumber) {
+        // Highlight selected star
+        for (let i = 1; i <= starNumber; i++) {
+            document.getElementById('rating' + i).checked = true;
+            document.querySelector(`label[for=rating${i}]`).classList.add('text-yellow-500');
+        }
 
+        // Un-highlight unselected stars
+        for (let i = starNumber + 1; i <= 5; i++) {
+            document.getElementById('rating' + i).checked = false;
+            document.querySelector(`label[for=rating${i}]`).classList.remove('text-yellow-500');
+        }
+    }
+</script>
 @section('content')
+
     <div style='background-color:rgba(0, 0, 0, 0)'>
         <div class="container px-5 py-24 mx-auto" style="cursor: auto;">
           <div class="lg:w-4/5 mx-auto flex flex-wrap">
@@ -68,7 +83,31 @@
                                             </button>
                                             <div class="relative overflow-hidden transition-all max-h-0 duration-700" style="" x-ref="container1" x-bind:style="selected == 1 ? 'max-height: ' + $refs.container1.scrollHeight + 'px' : ''">
                                                 <div class="px-10">
-                                                    <h1>dasda</h1>
+                                                    <div class="max-w-xl mx-auto mt-16 flex w-full flex-col border rounded-lg bg-white p-8">
+                                                        <h2 class="title-font mb-1 text-lg font-medium text-gray-900">Feedback</h2>
+                                                        <p class="mb-5 leading-relaxed text-gray-600">If you had any issues or you liked our product, please share
+                                                            with us!
+                                                        </p>
+                                                        <div class="mb-4">
+                                                            <div class="flex items-center">
+                                                                <label for="rating" class="mr-4">Note:</label>
+                                                                <div class="flex">
+                                                                  <!-- Utilisation d'un tableau pour générer les étoiles dynamiquement -->
+                                                                  @foreach(range(1, 5) as $i)
+                                                                    <input type="radio" name="rating" value="{{ $i }}" id="rating{{ $i }}" class="hidden" />
+                                                                    <label for="rating{{ $i }}" class="text-gray-600 hover:text-yellow-500 cursor-pointer">&#9733;</label>
+                                                                  @endforeach
+                                                                </div>
+                                                              </div>
+
+                                                        </div>
+                                                        <div class="mb-4">
+                                                            <label for="message" class="text-sm leading-7 text-gray-600">Message</label>
+                                                            <textarea id="message" name="message" class="h-32 w-full resize-none rounded border border-gray-300 bg-white py-1 px-3 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"></textarea>
+                                                        </div>
+                                                        <button class="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none">Send</button>
+                                                        <p class="mt-3 text-xs text-gray-500">Feel free to connect with us on social media platforms.</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </li>
@@ -84,13 +123,7 @@
                                             </button>
                                             <div class="relative overflow-hidden transition-all max-h-0 duration-700" style="" x-ref="container2" x-bind:style="selected == 2 ? 'max-height: ' + $refs.container2.scrollHeight + 'px' : ''">
                                                 <div class="px-10">
-                                                    <div class="flex flex-row-reverse justify-center p-10">
-                                                        <i class="bg-yellow-100 peer peer-hover:bg-yellow-500 hover:bg-yellow-500 w-12 h-12 mx-2">1</i>
-                                                        <i class="bg-yellow-100 peer peer-hover:bg-yellow-500 hover:bg-yellow-500 w-12 h-12 mx-2">2</i>
-                                                        <i class="bg-yellow-100 peer peer-hover:bg-yellow-500 hover:bg-yellow-500 w-12 h-12 mx-2">3</i>
-                                                        <i class="bg-yellow-100 peer peer-hover:bg-yellow-500 hover:bg-yellow-500 w-12 h-12 mx-2">4</i>
-                                                        <i class="bg-yellow-100 peer peer-hover:bg-yellow-500 hover:bg-yellow-500 w-12 h-12 mx-2">5</i>
-                                                      </div>
+                                                    <h1>dasda</h1>
                                                 </div>
                                             </div>
                                         </li>
@@ -103,6 +136,7 @@
           </div>
         </div>
     </div>
+
 
 
 @endsection
